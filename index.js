c0df24b30edd4a95dbd445b1249c7829c9328354
cdmitedaniel <cdmitedaniel@gmail.com>
2018-08-23 10:41:03 -0500

    static files

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.set('view engine','handlebars');
 
 app.get('/',(req,res) => res.render('home'));
 
+var fortune = require('./lib/fortune.js');
+
+app.get('/about',function(req,res){
+    res.render('about',{fortune:fortune.getFortune()
+    })
+})
+
+app.use(express.static(path.join(__dirname,'/public')));
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
