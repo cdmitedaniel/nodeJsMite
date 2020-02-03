882e1d95803b3b7dbc420763ec99f893604b9b98
cdmitedaniel <cdmitedaniel@gmail.com>
2020-02-03 10:34:29 -0500

    Commit Data

--- db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_USER', "pkrhnjnjckdfqm"); // Usuario
-define('DB_PASSWORD', "2e1398cff03acf9ca1be174bfca8ee9f4a97c8ba8cce21d0677dc67662e256cb"); // Password
-define('DB_DATABASE', "d972vla3ojlhmi"); // Nombre de la base de datos
-define('DB_SERVER', "ec2-184-73-176-11.compute-1.amazonaws.com"); //host server
-?>
